1ef2f77b72ea7e56657e3111b9047bb8e113527b
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2018-07-03 21:36:26 +0100

    modified the admin options

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'about',
     'contact',
     'skills',
+    'project'
 ]
 
 MIDDLEWARE = [
--- skills/admin.py
+++ skills/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Skill
 
+@admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
     list_display = 'name', 'get_expertise'
 
